f0683e278bba13839f02048e915bd6256ea076c4
ezzakibr <brahim.ezzaki@outlook.fr>
2025-01-17 04:33:48 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # AWS configs
-AWS_REGION = 'eu-west-3'  # Paris region based on your cluster
-S3_BUCKET = 'retail-analytics-project-2025'  # Your S3 bucket name
-DATABASE_NAME = 'retail_raw_db'  # Glue Catalog database name
-CRAWLER_ROLE = 'role_glue'
+AWS_REGION = '<your-aws-region>'
+S3_BUCKET = '<your-s3-bucket>'
+DATABASE_NAME = '<your-glue-database>'
+CRAWLER_ROLE = '<your-glue-role>'
 
 # Redshift configs
-REDSHIFT_HOST = 'retail-analytics-dw.cg8wdztehtjo.eu-west-3.redshift.amazonaws.com'
+REDSHIFT_HOST = '<your-redshift-endpoint>'
 REDSHIFT_PORT = '5439'
-REDSHIFT_DB = 'retail_analytics'  # Your existing database name
-REDSHIFT_USER = 'admin'
-REDSHIFT_PASSWORD = 'Strongpassword098!'  # You'll need to put your actual password here
-REDSHIFT_ROLE = 'arn:aws:iam::010438482524:role/RedshiftS3AccessRole'
+REDSHIFT_DB = '<your-redshift-database>'
+REDSHIFT_USER = '<your-redshift-username>'
+REDSHIFT_PASSWORD = '<your-redshift-password>'
+REDSHIFT_ROLE = '<your-redshift-role-arn>'
